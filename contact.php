4455c3ef99915859026ba98e9510a21ea40b42f1
bogdaniftode <bogdaniftode@gmail.com>
2019-07-26 15:11:11 +0300

    no message

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+ $name=$_POST['visitor_name'];
+ $email=$_POST['visitor_email'];
+ $country=$_POST['chose_country'];
+ $message=$_POST['visitor_message'];
+ $formcontent=" From: $name \n Email: $email \n From Country: $country \n Message: $message";
+ $recipient = "bogdaniftode@gmail.com";
+ $subject = "Contact from Iftode Bogdan-Webistie";
+ $mailheader="From: $email \r\n";
+ mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+ echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+ ?>
